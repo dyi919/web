cfb4448e3992ea0ea4a1d99c5adf25242094404b
Dongun Ned Yi <ehddjs13579@gmail.com>
2020-03-24 19:56:25 +0900

    added js file, jQuery

---
+++ colors.js
@@ -0,0 +1,38 @@
+var Links = {
+  setColor:function(color){
+//     var alist = document.querySelectorAll('a');
+//     for(a of alist) a.style.color = color;
+    $('a').css('color', color);
+  }
+}
+
+var Body = {
+  setColor: function(color){
+    // document.querySelector('body').style.color = color;
+    $('body').css('color', color)
+  },
+  
+  setBackgroundColor: function(color){
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
+  }
+}
+
+function nightDayHandler(self){
+  var target = document.querySelector('body');
+
+  if(self.value === 'night'){
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+
+    Links.setColor('lightpink');
+  } 
+  else{
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+
+    Links.setColor('blue');
+  }
+}
